e9d0fb57ac5bca9a0249cb976bfebcdbfa201a8a
MATTPC\Matt <matthewp214@gmail.com>
2019-03-10 17:33:25 -0500

    Update search results on field change

--- Storefront 2.0/web/search.js
+++ Storefront 2.0/web/search.js
@@ -1,5 +1,14 @@
 $(function() {
+    //Trigger on form submit or field change
     $('#search').on('submit', function(e){
+        search(e);
+    });
+    $('#searchField').on('change', function(e){
+        search(e);
+    });
+
+    //Hide all products then show relevant results
+    var search = function(e) {
         e.preventDefault();
         $('.product-card').each( function() {
             $(this).parent().hide();
@@ -7,5 +16,5 @@ $(function() {
                 $(this).parent().show();
             }
         });
-    });
+    }
 });
